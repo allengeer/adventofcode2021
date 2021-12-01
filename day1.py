c09af47d2919aa9c20e28f1e5092ab4f05cfb878
Allen Geer <allengeernz@gmail.com>
2021-12-01 19:03:14 +1300

    Day 1 complete!

--- day1.py
+++ day1.py
@@ -8,8 +8,23 @@ def name():
 
 
 def solve1():
-    return ""
+    input_list = utils.read_file_matrix("inputs/day1_1.txt", dtype=int)
+    count = 0
+    prev = input_list[0]
+    for reading in input_list[1:]:
+        if reading > prev:
+            count += 1
+        prev = reading
+    return count
 
 
 def solve2():
-    return ""
+    input_list = utils.read_file_matrix("inputs/day1_1.txt", dtype=int)
+    count = 0
+    for i in range(len(input_list) - 3):
+        window_a = input_list[i] + input_list[i+1] + input_list[i+2]
+        window_b = input_list[i+1] + input_list[i + 2] + input_list[i + 3]
+        if window_b > window_a:
+            count += 1
+
+    return count
