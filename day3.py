b8df7168a11e068859772641ffc2ded142feb5e9
Allen Geer <allengeernz@gmail.com>
2021-12-04 11:03:11 +1300

    Day 3 first go.

---
+++ day3.py
@@ -0,0 +1,46 @@
+import utils
+import statistics
+import numpy as np
+
+
+# test_array = utils.read_file_list_of_strings("inputs/test_file.txt")
+# test_matrix = utils.read_file_matrix("inputs/test_file.txt", dtype=str)
+
+
+def name():
+    return "Day 3"
+
+
+def solve1():
+    reading = utils.read_file_matrix("inputs/day3_1.txt", dtype=str)
+    reading = np.array([list(r) for r in reading])
+    gamma = "".join(str(i) for i in [statistics.mode(row) for row in reading.astype(int).transpose()])
+    epslion = "".join(str(int(not bool(i))) for i in [statistics.mode(row) for row in reading.astype(int).transpose()])
+    return int(gamma, 2) * int(epslion, 2)
+
+
+def solve2():
+    reading = utils.read_file_matrix("inputs/day3_1.txt", dtype=str)
+    reading = np.array([list(r) for r in reading])
+    o2rating = 0
+    co2rating = 0
+    for i in range(len(reading[0])):
+        oxyfilters = [max(statistics.multimode(row)) for row in reading.astype(int).transpose()]
+        reading = np.array(list(filter(lambda x: int(x[i]) == oxyfilters[i],
+                                       reading)))
+        if len(reading) == 1:
+            o2rating = int("".join(reading[0]), 2)
+            break
+    reading = utils.read_file_matrix("inputs/day3_1.txt", dtype=str)
+    reading = np.array([list(r) for r in reading])
+
+    for i in range(len(reading[0])):
+        co2filters = [ int(not bool(max(statistics.multimode(row)))) for row in reading.astype(int).transpose()]
+        reading = np.array(list(filter(lambda x: int(x[i]) == co2filters[i],
+                                       reading)))
+        if len(reading) == 1:
+            co2rating = int("".join(reading[0]), 2)
+            break
+
+    print(co2rating, o2rating)
+    return co2rating * o2rating
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-import day1,day2#,day3,day4,day5,day6,day7,day8,day9,day10,day11,day12,day13,day14,day15,day16,day17,day18,day19,day20,day21,day22,day23,day24,day25
-modules = [day1,day2]#,day3,day4,day5,day6,day7,day8,day9,day10,day11,day12,day13,day14,day15,day16,day17,day18,day19,day20,day21,day22,day23,day24,day25]
+import day1,day2,day3#,day4,day5,day6,day7,day8,day9,day10,day11,day12,day13,day14,day15,day16,day17,day18,day19,day20,day21,day22,day23,day24,day25
+modules = [day1,day2,day3]#,day4,day5,day6,day7,day8,day9,day10,day11,day12,day13,day14,day15,day16,day17,day18,day19,day20,day21,day22,day23,day24,day25]
 
 if __name__ == '__main__':
     for day in modules:
